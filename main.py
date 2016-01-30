a4af73d5321ad1a1c164c5cc177e8f1b0a186fb6
Teemu Leivo <teemu.leivo@gofore.com>
2016-01-30 17:30:06 +0200

    0,5x slowmotion

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
 from moviepy.editor import *
 
-# open two video files / subclips
-video1 = VideoFileClip("test.mp4").subclip(2,5)
-video2 = VideoFileClip("test.mp4").subclip(8,12)
-
-# combine them
-final_clip = concatenate_videoclips([video1,video2])
-
-# write to mp4 / gif
-#video.write_videofile("new.mp4", fps=25)
-final_clip.write_gif("new.gif", fps=25)
-
 '''
 Combines several video clips into one. Creates a new file and returns the path.
 @Param: file_list Several file paths as a list where the files are located
@@ -20,10 +9,10 @@ Combines several video clips into one. Creates a new file and returns the path.
 def combine_clips(file_list, output_file_name):
     video_list = []
     for file_name in file_list:
-        video = VideoFileClip(file_name)
+        video = VideoFileClip('videos/' + file_name)
         video_list.append(video)
-    final_clip = concatenate_videoclips(video_list)
-    final_clip.write_videofile(output_file_name)
+    final_clip = concatenate_videoclips(video_list).subfx(lambda c: c.speedx(0.5)) #0.5X slowdown
+    final_clip.write_videofile('videos/' + output_file_name)
     return True
 
 combine_clips(["test.mp4", "test.mp4", "test.mp4", "test.mp4", "test.mp4", "test.mp4", "test.mp4", "test.mp4", "test.mp4", "test.mp4", "test.mp4"], "iso_video.mp4")
